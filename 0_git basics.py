39efdd5cad13a6c95a58de33729049f79113f579
KripaDhamecha <kripadhamecha380@gmail.com>
2026-07-05 15:19:23 +0530

    git basics

---
+++ 0_git basics.py
@@ -0,0 +1,10 @@
+#github basics 
+#git is a version management system that allows developers to track changes in their codebase, collaborate with others, and manage different versions of their projects. It is widely used in software development and provides features such as branching, merging, and commit history.
+#git command - to add git files on staging 
+#git add [file1name] [file2name] 
+# or
+#git add .  
+#git commit -u(untracked files)
+#git commit -m "commit message"
+#git push origin main(branch name)
+#git pull origin main(branch name) - to fetch data from internet and merge it with local repository
